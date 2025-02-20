867ba48cf8b8b4f4ecad9b59f12b77798513a9f2
MrVaylis <mrvaylis@icloud.com>
2025-02-21 02:50:32 +0300

    28.09.2023

---
+++ js/config.js
@@ -0,0 +1,45 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 1.5, 
+
+  // Заголовок
+  title: 'PinkFace.xyz',
+
+  // Социальные сети
+  social: {
+    telegram: '@pinkfaces',
+    steam: 'pinkface',
+    vk: 'pinkface', 
+  }, 
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#1E90FF',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#00BFFF',
+  // Цвет тени кнопки play
+  playButtonShadowColor: 'rgba(255, 255, 255, 0.5)',
+
+
+  // Цвет заголовка
+  titleColor: '#00BFFF',
+  // Цвет тени загаловка
+  titleShadowColor: 'rgba(255, 255, 255, 0.5)',
+
+
+  // Текст копирайта
+  copyrightText: 'by - pinkface',
+  // Цвет копирайта
+  copyrightTextColor: '#0000CD',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#00BFFF',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: 'rgba(255, 255, 255, 0.5)',
+}
